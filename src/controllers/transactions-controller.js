02abb6c9b859d667e2673b7453f2a2fef4108ccb
Tainara <tainara_rafaely@hotmail.com>
2025-06-09 16:57:19 -0300

    Alteração de transações implementada

--- src/controllers/transactions-controller.js
+++ src/controllers/transactions-controller.js
@@ -6,17 +6,17 @@ export async function postTransaction(req, res) {
     const transaction = req.body;
     const user = res.locals.user;
 
-        try {
-            await db.collection("transactions").insertOne({
-                ...transaction,
-                userId: user._id,
-                createdAt: new Date()
-            });
-            res.sendStatus(201)
-        }
-        catch (err) {
-            res.status(500).send(err.message)
-        }
+    try {
+        await db.collection("transactions").insertOne({
+            ...transaction,
+            userId: user._id,
+            createdAt: new Date()
+        });
+        res.sendStatus(201)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
 };
 
 export async function getTransactions(req, res) {
@@ -24,16 +24,47 @@ export async function getTransactions(req, res) {
     const limit = 10;
     const start = (page - 1) * limit;
 
-    if (pagina <= 0) return res.sendStatus(400);
+    if (page <= 0) return res.sendStatus(400);
 
     try {
         const transactions = await db.collection("transactions")
-        .find()
-        .skip(start)
-        .limit(limit)
-        .toArray();
+            .find()
+            .skip(start)
+            .limit(limit)
+            .toArray();
         return res.send(transactions)
     } catch (error) {
         return res.status(500).send(error.message)
     }
 };
+
+export async function putTransactions(req, res) {
+    const { id } = req.params;
+    const transaction = req.body;
+    const userId = res.locals.user._id;
+
+    try {
+        const existingTransaction = await db.collection("transactions").findOne({
+            _id: new ObjectId(id),
+            userId: userId
+        });
+
+        if (!existingTransaction) {
+            return res.status(401).send("Não autorizado ou transação não encontrada");
+        }
+        await db.collection("transactions").updateOne({
+            _id: new ObjectId(id)
+        }, {
+            $set: {
+                value: transaction.value,
+                description: transaction.description,
+                type: transaction.type
+            }
+        })
+
+        res.sendStatus(204);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+
+}
--- src/routers/transactions-router.js
+++ src/routers/transactions-router.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { validarSchema } from "../middleware/schema-middleware.js";
-import { getTransactions, postTransaction } from "../controllers/transactions-controller.js";
+import { getTransactions, postTransaction, putTransactions } from "../controllers/transactions-controller.js";
 import { transactionSchema } from "../schemas/transaction-schema.js";
 import { validarToken } from "../middleware/auth-middleware.js";
 
 const transactionsRouter = Router();
 
 transactionsRouter.use(validarToken);
+
 transactionsRouter.post("/transactions", validarSchema(transactionSchema), postTransaction);
 transactionsRouter.get("/transactions", getTransactions);
+transactionsRouter.put("/transactions/:id", validarSchema(transactionSchema), putTransactions);
 
 
 export default transactionsRouter;
